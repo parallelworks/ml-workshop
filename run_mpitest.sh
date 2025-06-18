19067acbe01625a60d5785377610d66c176a83b1
StefanGary <stefanfgary@gmail.com>
2025-06-18 10:09:46 +0000

    Testing path/execution context for MPI test via script_submitter

--- run_mpitest.sh
+++ run_mpitest.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #===========================
 
+# Where am I?
+hostname
+pwd
+
 # Where is OpenMPI?
 export PATH="/pw/apps/ompi/bin":${PATH}
 
 # Compile
-mpicc -o mpitest.out mpitest.c
+mpicc -o mpitest.out ~/ml-workshop/mpitest.c
 
 # Run
 srun -N 2 -n 4 --mpi=pmi2 mpitest.out
